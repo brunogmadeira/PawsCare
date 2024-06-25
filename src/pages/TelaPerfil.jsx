a10a638932039c7cd170ed111ad427dea0c4e7bc
Yasmin Michels <yamichels@gmail.com>
2024-06-25 15:18:25 -0300

    Ajustes gerais

---
+++ src/pages/TelaPerfil.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Perfil from "../components/Perfil";
+
+const TelaPerfil = () => {
+    return(
+        <div>
+            <Header/>
+            <Perfil/>
+        </div>
+    )
+}
+export default TelaPerfil
